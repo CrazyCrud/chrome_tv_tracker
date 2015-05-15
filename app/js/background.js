f02323e4df5c11b5ebd398e17691c5da94eedb0d
CrazyCrud <constantin.lehenmeier@stud.uni-regensburg.de>
2015-05-15 14:27:34 +0200

    A show cannot be added a second time

--- app/js/background.js
+++ app/js/background.js
@@ -1,24 +1,3 @@
 chrome.browserAction.onClicked.addListener(function() {
-	console.log("Background!", "onClick", chrome.runtime);
 
-	chrome.browserAction.setPopup({
-		popup: 'popup.html'
-	});
-
-	chrome.alarms.create('watchnext-alarm', {
-		when: Date.now()
-	});
-
-	/*
-	chrome.runtime.connect({
-
-	});
-	*/
-});
-
-
-chrome.alarms.onAlarm.addListener(function(){
-	console.log("onAlarm");
-
-	
 });
